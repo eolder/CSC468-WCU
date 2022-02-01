53d72b5a8d9e03de27a0e26aded225b752bbb589
Evan Older <evanmolder@gmail.com>
2022-02-01 08:19:04 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,22 +8,16 @@ request = portal.context.makeRequestRSpec()
 nodes = [request.XenVM("node")]
 
 # List of packages to install on each node.
-updstr = "sudo apt install -y"
-packages = [
-  "apache2",
-  "htop",
-  "tmux",
-]
-for x in packages:
-  updstr += " " + x
+updstr = "sudo apt install -y apache2 htop tmux git docker.io curl"
 
 # Create the nodes and handle updates and initial setup
 for node in nodes:
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   node.routable_control_ip = "true"
-  node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update && sudo apt upgrade -y"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="apt update && sudo apt upgrade -y"))
   node.addService(rspec.Execute(shell="/bin/sh", command=updstr))
-  node.addService(rspec.Execute(shell="/bin/sh", command="sudo systemctl status apache2"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="git clone https://github.com/eolder/CSC468-WCU.git"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="tmux new-session -d -s kubernetes 'bash CSC468-WCU/setup"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
